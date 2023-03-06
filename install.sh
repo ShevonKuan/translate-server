64dcec5abed7e654625bc9d8f044e7c9341a7368
ShevonKuan <331749486@qq.com>
2023-03-06 16:25:20 +0800

    add systemd config

---
+++ install.sh
@@ -0,0 +1,19 @@
+# for linux x86_64
+install_deepl(){
+    last_version=$(curl -Ls "https://api.github.com/repos/ShevonKuan/deepl-server/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+    if [[ ! -n "$last_version" ]]; then
+        echo -e "${red}Failed to detect DeepL version, probably due to exceeding Github API limitations.${plain}"
+        exit 1
+    fi
+    echo -e "DeepL latest version: ${last_version}, Start install..."
+    wget -q -N --no-check-certificate -O /usr/bin/deepl https://github.com/OwO-Network/DeepLX/releases/download/${last_version}/DeepLServer-linux-amd64
+
+
+    chmod +x /usr/bin/deeplx
+    wget -q -N --no-check-certificate -O /etc/systemd/system/deepl.service https://raw.githubusercontent.com/ShevonKuan/deepl-server/main/deepl.service
+    systemctl daemon-reload
+    systemctl enable deepl
+    systemctl start deepl
+    echo -e "Installed successfully, listening at 0.0.0.0:1188"
+}
+install_deepl
